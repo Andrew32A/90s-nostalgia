a2282399059d0c4dadfbc1a710c6322b576c8b00
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-05 19:16:10 -0800

    added ease out expo animations

---
+++ js/welcome.js
@@ -0,0 +1,26 @@
+anime({
+	targets: '.card',
+	opacity: 1,
+	duration: 1800,
+	translateY: ["-30px", "0px"],
+	easing: "easeOutExpo",
+	delay: (el, i) => 200 * i,
+})
+
+anime({
+	targets: 'h1',
+	opacity: 1,
+	duration: 1800,
+	translateY: ["-30px", "0px"],
+	easing: "easeOutExpo",
+	delay: (el, i) => 200 * i,
+})
+
+anime({
+	targets: 'p',
+	opacity: 1,
+	duration: 1800,
+	translateY: ["-30px", "0px"],
+	easing: "easeOutExpo",
+	delay: (el, i) => 200 * i,
+})
